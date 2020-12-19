75bd493dbe9b154e0d264e7dd33d84c5930644da
Azahar Hossain <azahar.hossain10@yahoo.com>
2020-12-19 17:09:01 +0100

    Annotated the main Application

--- src/main/java/com/myproject/springbootpractise/SpringbootPractiseApplication.java
+++ src/main/java/com/myproject/springbootpractise/SpringbootPractiseApplication.java
@@ -1,9 +1,13 @@
 package com.myproject.springbootpractise;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class SpringbootPractiseApplication {
 
 	public static void main(String[] args) {
