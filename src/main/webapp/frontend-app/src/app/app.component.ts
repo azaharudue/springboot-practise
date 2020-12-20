d111a00cf58b3418e0e824e89050cc9cd9a3f35f
Azahar Hossain <azahar.hossain10@yahoo.com>
2020-12-20 10:52:52 +0100

    Show hard coded JSON (Mock) data in a table

--- src/main/webapp/frontend-app/src/app/app.component.ts
+++ src/main/webapp/frontend-app/src/app/app.component.ts
@@ -9,12 +9,14 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AppComponent implements OnInit {
   public submitted: boolean;
   roomsearch: FormGroup;
+  rooms: Room[];
   constructor(){
     this.submitted  = false;
     this.roomsearch = new FormGroup({
       checkin : new FormControl(''),
       checkout: new FormControl('')
    });
+    this.rooms = ROOMS;
   }
     ngOnInit(): void{
 
@@ -23,9 +25,39 @@ export class AppComponent implements OnInit {
     console.log(value);
     this.submitted = true;
   }
+  reserveRoom(value: string): void {
+    console.log(`Room id for reservation: ${value}`);
+
+  }
 
 }
 export interface RoomSearch{
   checkin: string;
   checkout: string;
 }
+export interface Room{
+  id: string;
+  roomNumber: string;
+  price: string;
+  links: string;
+}
+const ROOMS: Room[] = [
+  {
+    id: '556444442',
+    roomNumber: '401',
+    price: '120',
+    links: ''
+  },
+  {
+    id: '8648464784',
+    roomNumber: '410',
+    price: '100',
+    links: ''
+  },
+  {
+    id: '5648451',
+    roomNumber: '233',
+    price: '80',
+    links: ''
+  }
+];
