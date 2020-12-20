bcd415ede938c113c95a542a839b1a35348635a1
Azahar Hossain <azahar.hossain10@yahoo.com>
2020-12-20 10:31:08 +0100

    Created a reactive form

--- src/main/webapp/frontend-app/src/app/app.component.ts
+++ src/main/webapp/frontend-app/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend-app';
+export class AppComponent implements OnInit {
+  public submitted: boolean;
+  roomsearch: FormGroup;
+  constructor(){
+    this.submitted  = false;
+    this.roomsearch = new FormGroup({
+      checkin : new FormControl(''),
+      checkout: new FormControl('')
+   });
+  }
+    ngOnInit(): void{
+
+    }
+    onSubmit({value, valid}: {value: RoomSearch, valid: boolean}): void {
+    console.log(value);
+    this.submitted = true;
+  }
+
+}
+export interface RoomSearch{
+  checkin: string;
+  checkout: string;
 }
