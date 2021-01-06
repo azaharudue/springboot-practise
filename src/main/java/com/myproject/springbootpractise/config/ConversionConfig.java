1a6d9c9a290e5884cecbeb2e5e84eac11a7c4abc
Azahar Hossain <azahar.hossain10@yahoo.com>
2021-01-06 22:00:07 +0100

    Added in ConversionConfig Bean annotation and RoomEntity to ReservationResponse conversion
    Signed-off-by: Azahar Hossain <azahar.hossain10@yahoo.com>

--- src/main/java/com/myproject/springbootpractise/config/ConversionConfig.java
+++ src/main/java/com/myproject/springbootpractise/config/ConversionConfig.java
@@ -1,5 +1,7 @@
 package com.myproject.springbootpractise.config;
 
+import com.myproject.springbootpractise.converter.RoomEntityToReservationResponseConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ConversionServiceFactoryBean;
 import org.springframework.core.convert.ConversionService;
@@ -10,11 +12,14 @@ import java.util.Set;
 
 @Configuration
 public class ConversionConfig {
-    private Set<Converter> getConverters(){
+    private Set<Converter> getConverters() {
         Set<Converter> converters = new HashSet<Converter>();
+        converters.add(new RoomEntityToReservationResponseConverter());
         return converters;
     }
-    public ConversionService conversionService(){
+
+    @Bean
+    public ConversionService conversionService() {
         ConversionServiceFactoryBean bean = new ConversionServiceFactoryBean();
         bean.setConverters(getConverters());
         bean.afterPropertiesSet();
